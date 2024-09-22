221b2f1f9fd715817734cf7d64da8243bc9fa403
zxcvbnmq15 <1722355604@qq.com>
2024-09-22 12:17:56 +0800

    Add files via upload

---
+++ 打包.py
@@ -0,0 +1,43 @@
+import os
+
+def check_and_rename_file(folder_path):
+    # Traverse all subfolders and files in the folder
+    for root_directory, directories, file_name_list in os.walk(folder_path):
+        for file_name in file_name_list:
+            # Get the full path of the file
+            full_path = os.path.join(root_directory, file_name)
+            
+            try:
+                # Read the file content
+                with open(full_path, 'r', encoding='latin-1') as file:
+                    content = file.read()
+                
+                # Determine the new file suffix based on the file content
+                if '/Script/Engine' in content:
+                    new_suffix = '.dat'
+                elif 'LuaS' in content:
+                    new_suffix = '.dat'
+                elif 'BKHD' in content:
+                    new_suffix = '.dat'
+                elif 'if ... then' in content:
+                    new_suffix = '.dat'
+                elif 'function' in content:
+                    new_suffix = '.dat'
+                else:
+                    new_suffix = '.dat'
+                
+                # Split filename and current suffix
+                basename, extension = os.path.splitext(file_name)
+                new_name = basename + new_suffix
+                new_full_path = os.path.join(root_directory, new_name)
+                
+                # If new name is different from current name, rename file and print modified file
+                if new_full_path != full_path:
+                    os.rename(full_path, new_full_path)
+                    print(f"SET_FILE REACK {new_name}")
+            except Exception as e:
+                print(f"Error processing file {file_name}: {e}")
+
+# Get folder path entered by user
+input_path = "/sdcard/Download/repack_folder"
+check_and_rename_file(input_path)
